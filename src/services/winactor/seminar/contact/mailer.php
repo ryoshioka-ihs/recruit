f5f5e07c6adf20cc74471552bb402a89b990c104
ryoshioka <ryoshioka@iimhs.co.jp>
2018-04-10 15:26:14 +0900

    セミナーページにかかる修正

--- src/services/winactor/seminar/contact/mailer.php
+++ src/services/winactor/seminar/contact/mailer.php
@@ -32,21 +32,16 @@ $q6 = $_POST['q6'];
 
 //お客様情報
 $company = $_POST['company'];//会社名
-$name1 = $_POST['name1'];//参加者①
-$despartment1 = $_POST['despartment1'];//部署名①
-$position1 = $_POST['position1'];//役職①
-$emailadd1 = $_POST['email1'];//メールアドレス①
-$tel1 = $_POST['tel1'];//	TEL①
-$name2 = $_POST['name2'];//参加者②
-$despartment2 = $_POST['despartment2'];//部署名②
-$position2 = $_POST['position2'];//役職②
-$emailadd2 = $_POST['email2'];//メールアドレス②
-$tel2 = $_POST['tel2'];//  TEL②
-$company_size = $_POST['company_size'];//会社規模
-$seminar = $_POST['seminar'];//	セミナー日程
-$enquete = $_POST['enquete'];// アンケート
-$other = $_POST['other'];// アンケート-その他入力
-$request = $_POST['request'];// 特記事項
+$name = $_POST['name'];//参加者名
+$despartment = $_POST['despartment'];//部署
+$position = $_POST['position'];//役職
+$email = $_POST['email'];//メールアドレス
+$phone = $_POST['phone'];//電話番号
+$people = $_POST['people'];//従業員数
+$seminar = $_POST['seminar'];//セミナー
+$enquete = $_POST['enquete'];//セミナーをしったきっかけ
+$other = $_POST['other'];//その他を選択した人
+$message = $_POST['message'];//特記事項
 $check = $_POST['check'];// 個人情報の取扱
 
 
@@ -57,10 +52,10 @@ $email->addTo($to)->
        setFrom($from)->
        setFromName("WinActorセミナーのお申込み")->
        setSubject("【WinActorセミナーのお申込みを承りました。】$seminar" )->
-       setText("会社名: $company \r\n ご希望の開催日: $seminar \r\n 参加者①: $name1 \r\n部署名①: $despartment1 \r\n お役職①: $position1 \r\nEmailアドレス①: $emailadd1  \r\nTEL①: $tel1　\r\n 参加者②: $name2 \r\n 部署名②: $despartment2 \r\nお役職②: $position2 \r\nEmailアドレス②: $emailadd2  \r\n TEL②: $tel2　\r\n 会社規模: $company_size  　\r\nアンケート: $enquete　\r\nアンケート-その他入力: $other　\r\n その他特記事項: $request　\r\n個人情報の取扱: $check")->
+       setText("会社名: $company \r\n ご希望の開催日: $seminar \r\n 参加者名: $name \r\n部署名: $despartment \r\n お役職: $position \r\nメールアドレス①: $email  \r\n電話番号: $phone　\r\n 従業員数: $people  \r\nセミナーをしったきっかけ: $enquete　\r\nその他を選択した人: $other　\r\n 特記事項: $message　\r\n個人情報の取扱: $check")->
 
 
-       setHtml("<strong>会社名:</strong> $company<br /> <strong>ご希望の開催日:</strong> $seminar<br /><br /> <strong>参加者①:</strong> $name1<br /> <strong>部署名①:</strong> $despartment1<br /> <strong>お役職①:</strong> $position1<br /> <strong>Emailアドレス①:</strong> $emailadd1<br /> <strong>TEL①:</strong> $tel1<br /><br /> <strong>参加者②:</strong> $name2<br /> <strong>部署名②:</strong> $despartment2<br /> <strong>お役職②:</strong> $position2<br /> <strong>Emailアドレス②:</strong> $emailadd2<br /> <strong>TEL②:</strong> $tel2<br /><br /> <strong>会社規模:</strong> $company_size<br />   <strong>アンケート:</strong> $enquete<br /> <strong>アンケート-その他入力:</strong> $other<br /> <strong>その他特記事項:</strong> $request<br /> <strong>個人情報の取扱:</strong> $check<br />")->
+       setHtml("<strong>会社名:</strong> $company<br /> <strong>ご希望の開催日:</strong> $seminar<br /><br /> <strong>参加者名:</strong> $name<br /> <strong>部署名:</strong> $despartment<br /> <strong>お役職:</strong> $position<br /> <strong>メールアドレス:</strong> $email<br /> <strong>電話番号:</strong> $phone<br /><strong>従業員数:</strong> $people<br />   <strong>セミナーをしったきっかけ:</strong> $enquete<br /> <strong>その他を選択した人:</strong> $other<br /> <strong>特記事項:</strong> $message<br /> <strong>個人情報の取扱:</strong> $check<br />")->
        addCategory('contact');
 
 $response = $sendgrid->send($email);
