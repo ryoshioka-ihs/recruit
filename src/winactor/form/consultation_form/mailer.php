3deb1ebaea901d813e58bfbfa7e492496329ab6d
ryoshioka <ryoshioka@iimhs.co.jp>
2018-07-12 10:36:40 +0900

    総合お問い合わせにアンケートを追加

--- src/winactor/form/consultation_form/mailer.php
+++ src/winactor/form/consultation_form/mailer.php
@@ -14,6 +14,7 @@ $company = $_POST['company'];//会社名
 $name = $_POST['name'];//ご担当者名
 $emailAdress = $_POST['emailAdress'];//メールアドレス
 $phone = $_POST['phone'];//電話番号
+$enquete = $_POST['enquete'];//お問い合わせのきっかけ
 //$categoriy =$_POST['categoriy'];//ご相談のカテゴリー
 $message = $_POST['message'];//詳しく聞きたいこと
 $check = $_POST['check'];// 個人情報の取扱
@@ -45,10 +46,10 @@ $email->addTo($to)->
        setFrom($from)->
        //setFromName("WinActor資料DL")->
        setSubject("【WinActor総合お問合わせ】お問合わせを受け付けました。" )->
-       setText("下記の通りお問合わせを受け付けました。\r\n担当者は対応のほどよろしくお願いいたします。\r\n\r\n会社名: $company \r\n ご担当者名: $name \r\n　メールアドレス: $emailAdress  \r\n電話番号: $phone　\r\n　ご相談のカテゴリー: $categoriy \r\n詳しく聞きたいこと: $message\r\n 個人情報の取扱: $check")->
+       setText("下記の通りお問合わせを受け付けました。\r\n担当者は対応のほどよろしくお願いいたします。\r\n\r\n会社名: $company \r\n ご担当者名: $name \r\n　メールアドレス: $emailAdress  \r\n電話番号: $phone　\r\n　お問い合わせのきっかけ: $enquete \r\n詳しく聞きたいこと: $message\r\n 個人情報の取扱: $check")->
 
 
-       setHtml("WinActor総合お問合わせフォームより下記のお問合わせを受け付けました<br>ご担当者は対応のほどよろしくお願いいたします。<br><br><strong>会社名:</strong> $company<br /> <strong>ご担当者名:</strong> $name<br /> <strong>メールアドレス:</strong> $emailAdress<br /> <strong>電話番号:</strong> $phone<br /> <strong>ご相談のカテゴリー:</strong> $categoriy <br /><strong>詳しく聞きたいこと:</strong> $message<br /> <strong>個人情報の取扱:</strong> $check<br />")->
+       setHtml("WinActor総合お問合わせフォームより下記のお問合わせを受け付けました<br>ご担当者は対応のほどよろしくお願いいたします。<br><br><strong>会社名:</strong> $company<br /> <strong>ご担当者名:</strong> $name<br /> <strong>メールアドレス:</strong> $emailAdress<br /> <strong>電話番号:</strong> $phone<br /> <strong>お問い合わせのきっかけ:</strong> $enquete <br /><strong>詳しく聞きたいこと:</strong> $message<br /> <strong>個人情報の取扱:</strong> $check<br />")->
        addCategory('contact');
 
 $response = $sendgrid->send($email);
