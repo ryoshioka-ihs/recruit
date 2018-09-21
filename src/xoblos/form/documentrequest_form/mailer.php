bae462a60831b7be5542debd658dbe9a5d4d982b
ryoshioka <ryoshioka@iimhs.co.jp>
2018-09-21 13:04:43 +0900

    xoBlosページ作成

--- src/xoblos/form/documentrequest_form/mailer.php
+++ src/xoblos/form/documentrequest_form/mailer.php
@@ -11,9 +11,14 @@ $to                = $_ENV['TO'];
 
 //お客様情報
 $company = $_POST['company'];//会社名
-$name = $_POST['name'];//ご担当者名
+$despartment = $_POST['despartment'];//部署名
+$position = $_POST['position'];//お役職
+$name1 = $_POST['name1'];//姓
+$name2 = $_POST['name2'];//名
 $emailAdress = $_POST['emailAdress'];//メールアドレス
-$phone = $_POST['phone'];//電話番号
+$phone1 = $_POST['phone1'];//電話番号1
+$phone2 = $_POST['phone2'];//電話番号2
+$phone3 = $_POST['phone3'];//電話番号3
 $check = $_POST['check'];// 個人情報の取扱
 
 
@@ -24,10 +29,10 @@ $email->addTo($to)->
        setFrom($from)->
        //setFromName("IHSWinActor")->
        setSubject("【xoBlos資料がダウンロードされました】" )->
-       setText("営業各位\r\n\r\nxoBlosオファーページの資料がダウンロードされました。\r\n必要に応じてアクションをお願いいたします。\r\n\r\n会社名: $company \r\n ご担当者名: $name \r\n　メールアドレス: $emailAdress  \r\n電話番号: $phone　\r\n 個人情報の取扱: $check")->
+       setText("営業各位\r\n\r\nxoBlosオファーページの資料がダウンロードされました。\r\n必要に応じてアクションをお願いいたします。\r\n\r\n会社名: $company \r\n 部署名: $despartment \r\n お役職: $position \r\nご担当者名: $name1 $name2 様\r\n 　メールアドレス: $emailAdress \r\n電話番号: $phone1-$phone2-$phone3 \r\n 個人情報の取扱: $check")->
 
 
-       setHtml("営業各位<br><br>xoBlosオファーページの資料がダウンロードされました。<br />必要に応じてアクションをお願いいたします。<br /><br /><strong>会社名:</strong> $company<br /> <strong>ご担当者名:</strong> $name<br /> <strong>メールアドレス:</strong> $emailAdress<br /> <strong>電話番号:</strong> $phone<br /> <strong>個人情報の取扱:</strong> $check<br />")->
+       setHtml("営業各位<br><br>xoBlosオファーページの資料がダウンロードされました。<br />必要に応じてアクションをお願いいたします。<br /><br /><strong>会社名:</strong> $company <br /> <strong>部署名:</strong> $despartment <br /> <strong>お役職:</strong> $position <br /> <strong>ご担当者名:</strong> $name1 $name2 様<br /> <strong>メールアドレス:</strong> $emailAdress <br /> <strong>電話番号:</strong> $phone1-$phone2-$phone3 <br /> <strong>個人情報の取扱:</strong> $check <br />")->
        addCategory('contact');
 
 $response = $sendgrid->send($email);
