810bad21ff90cc4a0da677de4d11a4124c107128
ryoshioka <ryoshioka@iimhs.co.jp>
2018-10-12 17:32:15 +0900

    作り直し！！

--- src/xoblos/form/documentrequest_form/mailer.php
+++ src/xoblos/form/documentrequest_form/mailer.php
@@ -11,9 +11,12 @@ $to                = $_ENV['TO'];
 
 //お客様情報
 $company = $_POST['company'];//会社名
-$name = $_POST['name'];//ご担当者名
-$emailAdress = $_POST['emailAdress'];//メールアドレス
-$phone = $_POST['phone'];//電話番号
+$Department = $_POST['Department'];//部署名
+$Title = $_POST['Title'];//お役職
+$LastName = $_POST['LastName'];//姓
+$FirstName = $_POST['FirstName'];//名
+$Email = $_POST['Email'];//メールアドレス
+$phone = $_POST['Phone'];//電話番号
 $check = $_POST['check'];// 個人情報の取扱
 
 
@@ -24,10 +27,10 @@ $email->addTo($to)->
        setFrom($from)->
        //setFromName("IHSWinActor")->
        setSubject("【WinActor資料がダウンロードされました】" )->
-       setText("営業各位\r\n\r\nWinActorオファーページの資料がダウンロードされました。\r\n必要に応じてアクションをお願いいたします。\r\n\r\n会社名: $company \r\n ご担当者名: $name \r\n　メールアドレス: $emailAdress  \r\n電話番号: $phone　\r\n 個人情報の取扱: $check")->
+       setText("営業各位\r\n\r\nWinActorオファーページの資料がダウンロードされました。\r\n必要に応じてアクションをお願いいたします。\r\n\r\n会社名: $company \r\n 部署名: $Department \r\n お役職: $Title \r\nご担当者名: $LastName $FirstName 様 \r\n　メールアドレス: $Email \r\n電話番号: $Phone　\r\n 個人情報の取扱: $check")->
 
 
-       setHtml("営業各位<br><br>WinActorオファーページの資料がダウンロードされました。<br />必要に応じてアクションをお願いいたします。<br /><br /><strong>会社名:</strong> $company<br /> <strong>ご担当者名:</strong> $name<br /> <strong>メールアドレス:</strong> $emailAdress<br /> <strong>電話番号:</strong> $phone<br /> <strong>個人情報の取扱:</strong> $check<br />")->
+       setHtml("営業各位<br><br>WinActorオファーページの資料がダウンロードされました。<br />必要に応じてアクションをお願いいたします。<br /><br /><strong>会社名:</strong> $company<br /> <strong>部署名:</strong> $Department<br /> <strong>お役職:</strong> $Title<br /><strong>ご担当者名:</strong> $LastName $FirstName 様<br /> <strong>メールアドレス:</strong> $Email<br /> <strong>電話番号:</strong> $Phone<br /> <strong>個人情報の取扱:</strong> $check<br />")->
        addCategory('contact');
 
 $response = $sendgrid->send($email);
@@ -35,5 +38,5 @@ var_dump($response);
 
 
 // 正常終了時にthanks.htmlへリダイレクト
-header('Location: https://www.iimhs.co.jp/winactor/form/documentrequest_form/thanks.html');
+header('Location: /xoblos/form/documentrequest_form/thanks.html');
 exit();
