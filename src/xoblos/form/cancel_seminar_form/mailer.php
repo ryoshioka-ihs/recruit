cf5c86b53e4242233cafa35105020011b139676e
ryoshioka <ryoshioka@iimhs.co.jp>
2018-11-30 13:12:20 +0900

    諸々修正

--- src/xoblos/form/cancel_seminar_form/mailer.php
+++ src/xoblos/form/cancel_seminar_form/mailer.php
@@ -30,11 +30,11 @@ $email    = new SendGrid\Email();
 $email->addTo($emailAdress)->
        setFrom($from)->
        setFromName("IIMヒューマン・ソリューション株式会社")->
-       setSubject("【WinActorセミナーのキャンセルを承りました】" )->
-       setText("$Company \r\n$Lastname $Fisrtname 様\r\n\r\nご連絡いただきまして、ありがとうございます。\r\n下記内容でお申し込みのキャンセルを承りました。\r\n\r\n　会社名: $Company \r\n ご希望の開催日: $Seminar \r\n メールアドレス: $EmailAdress \r\n　電話番号: $Phone \r\n　特記事項: $Message \r\n　個人情報の取扱: $check \r\n\r\n　別日程でのご参加をご希望の方は、\r\n https://www.iimhs.co.jp/winactor/form/seminar_form/ よりお申し込みください。\r\n\r\n 有償セミナーの場合は、担当より、別途確認のご連絡をさせていただきます。")->
+       setSubject("【xoBlos製品概要セミナーのキャンセルを承りました】" )->
+       setText("$Company \r\n$Lastname $Fisrtname 様\r\n\r\nご連絡いただきまして、ありがとうございます。\r\n下記内容でお申し込みのキャンセルを承りました。\r\n\r\n　会社名: $Company \r\n ご希望の開催日: $Seminar \r\n メールアドレス: $EmailAdress \r\n　電話番号: $Phone \r\n　特記事項: $Message \r\n　個人情報の取扱: $check \r\n\r\n　別日程でのご参加をご希望の方は、\r\n https://www.iimhs.co.jp/xoblos/form/seminar_form/ よりお申し込みください。\r\n\r\n")->
 
 
-       setHtml("$Company <br> $Lastname $Firstname 様<br><br>ご連絡いただきまして、ありがとうございます。<br>下記内容でお申し込みのキャンセルを承りました。<br><br>会社名: $Company <br />ご希望の開催日： $Seminar <br />メールアドレス: $EmailAdress <br />電話番号: $Phone <br />特記事項: $Message <br />個人情報の取扱: $check <br /><br>別日程でのご参加をご希望の方は<br> https://www.iimhs.co.jp/winactor/form/seminar_form/ よりお申し込みください。<br><br>有償セミナーの場合は、担当より、別途確認のご連絡をさせていただきます。")->
+       setHtml("$Company <br> $Lastname $Firstname 様<br><br>ご連絡いただきまして、ありがとうございます。<br>下記内容でお申し込みのキャンセルを承りました。<br><br>会社名: $Company <br />ご希望の開催日： $Seminar <br />メールアドレス: $EmailAdress <br />電話番号: $Phone <br />特記事項: $Message <br />個人情報の取扱: $check <br /><br>別日程でのご参加をご希望の方は<br> https://www.iimhs.co.jp/xoblosform/seminar_form/ よりお申し込みください。<br><br>")->
        addCategory('contact');
 
 $response = $sendgrid->send($email);
@@ -46,7 +46,7 @@ $email    = new SendGrid\Email();
 $email->addTo($to)->
        setFrom($from)->
        //setFromName("")->
-       setSubject("【WinActorセミナーのキャンセルを承りました】" )->
+       setSubject("【xoBlos製品概要セミナーのキャンセルを承りました】" )->
        setText("下記内容にてセミナーのキャンセルを承りました。\r\nご担当者はご対応のほどよろしくお願いいたします。\r\n\r\n会社名: $Company \r\n ご希望の開催日: $Seminar\r\n 参加者名: $Lastname $Firstname 様\r\n メールアドレス: $EmailAdress\r\n　電話番号: $Phone \r\n　特記事項: $Message\r\n　個人情報の取扱: $check")->
 
 
@@ -57,6 +57,6 @@ $response = $sendgrid->send($email);
 var_dump($response);
 
  //正常終了時にthanks.htmlへリダイレクト
-header('Location: /winactor/form/cancel_seminar_form/thanks.html');
+header('Location: /xoblos/form/cancel_seminar_form/thanks.html');
 exit();
 
