f69e974283b8d401c059da0694aca016f3d4a0a5
ryoshioka <ryoshioka@iimhs.co.jp>
2019-04-11 19:57:31 +0900

    ファイル修正

--- src/js/seminar.js
+++ src/js/seminar.js
@@ -57,26 +57,37 @@ function hideFinishedDateSeminar() {
 //======================================================================================================
 function hideFinishedDateSeminarStop() {
 	$sessions = jQuery('.session .apply');
+	$sessions_status = jQuery('.session .status');
+
+
 	if ( $sessions.length <= 0 ) {
 		return;
 	}
 
 	var today = new Date();
-	var status = ('.status');//statusの位置を取得
+	
 
+	
 	$sessions.each( function() {
+
 		var apply  = $(this).text();
+//		var apply = $('.apply').text();
 		var match = apply.match(/([0-9]{4})年([0-9]{1,2})?月([0-9]{1,2})?日/);
 		var year  = match[1];
 		var month = Number(match[2]) - 1; /* JSのDateは月を0-11で扱うため調整 */
 		var day   = Number(match[3]) + 1; /* 当日の日付まで表示するため調整 */
 		var session_apply = new Date(year, month, day);
+		var status = ('.status');//statusの位置を取得
 
 		if (today.getTime() > session_apply.getTime()) {
-			$('.status').closest('td').text('受付終了');//これを有効にすると、全部のstatusが受付終了にされてしまう
-			//$(this).closest('td').text('受付終了');//これを有効にすると、終了した締切日に受付状況「受付終了」が表示される
-			}
-
+	
+			$sessions_status.each( function() {	
+				$(this).closest('td').text('受付終了');//これを有効にすると、終了した締切日に受付状況「受付終了」が表示される
+				//$('.status').closest('td').text('受付終了');//これを有効にすると、全部のstatusが受付終了にされてしまう
+				//$('.apply').closest('td').text('受付終了');//これを有効にすると、終了した締切日に受付状況「受付終了」が表示される
+				//$(this).$('.status').closest('td').text('受付終了');//これを有効にすると、終了した締切日に受付状況「受付終了」が表示される
+			});
+		};
 	});
 }
 
