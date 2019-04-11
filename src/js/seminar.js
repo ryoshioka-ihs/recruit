d1755224c99950bf83bb780fac9d5e1d4dd1f575
ryoshioka <ryoshioka@iimhs.co.jp>
2019-04-11 17:03:18 +0900

    コード修正

--- src/js/seminar.js
+++ src/js/seminar.js
@@ -57,13 +57,12 @@ function hideFinishedDateSeminar() {
 //======================================================================================================
 function hideFinishedDateSeminarStop() {
 	$sessions = jQuery('.session .apply');
-	$sessions_status = jQuery('.session .status')
 	if ( $sessions.length <= 0 ) {
 		return;
 	}
 
 	var today = new Date();
-
+	var status = ('.status');//statusの位置を取得
 
 	$sessions.each( function() {
 		var apply  = $(this).text();
@@ -72,16 +71,15 @@ function hideFinishedDateSeminarStop() {
 		var month = Number(match[2]) - 1; /* JSのDateは月を0-11で扱うため調整 */
 		var day   = Number(match[3]) + 1; /* 当日の日付まで表示するため調整 */
 		var session_apply = new Date(year, month, day);
-		var status = ('.session .status');//statusの位置を取得
-
 
 		if (today.getTime() > session_apply.getTime()) {
-			$sessions_status.closest('td').text('受付終了');//これを有効にすると、全部のstatusが受付終了にされてしまう
-			//jQuery('.session .status').text('受付終了');//これを有効にすると、終了した締切日に受付状況「受付終了」が表示される
+			$('.status').closest('td').text('受付終了');//これを有効にすると、全部のstatusが受付終了にされてしまう
+			//$(this).closest('td').text('受付終了');//これを有効にすると、終了した締切日に受付状況「受付終了」が表示される
 			}
 
 	});
 }
+
 
 
 
