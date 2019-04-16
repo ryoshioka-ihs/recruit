c6a16a05ddc6ee6b8f1f9fc5fb25ca052c643679
ryoshioka <ryoshioka@iimhs.co.jp>
2019-04-16 09:28:04 +0900

    スクリプト修正

---
+++ src/js/view_timer.js
@@ -0,0 +1,36 @@
+  //======================================================================================================
+// 
+// 機能  ：セミナー受付終了日の翌日になったらプルダウンを非表示（セミナーお申込みフォーム）
+// 引数  ：
+// 戻り値：
+//======================================================================================================
+function hideFinishedDateSeminarForm() {
+  //$sessions = jQuery('.seminar_day');
+  //if ( $sessions.length <= 0 ) {
+  //  return;
+  //}
+
+  //var today = new Date();
+  var type = ('.title');
+  alert (type);
+
+
+//$sessions.each(function() {
+
+
+  //var wrap = $(this).parents().attr("class");
+
+  //  var match = type.match(/([0-9]{4})年([0-9]{1,2})?月([0-9]{1,2})?日/);
+  //  var year  = match[1];
+  //  var month = Number(match[2]) - 1; /* JSのDateは月を0-11で扱うため調整 */
+  //  var day   = Number(match[3]) + 1; /* 当日の日付まで表示するため調整 */
+  //  var session_date = new Date(year, month, day);
+  
+  //
+  //if(today.getTime() => session_date.getTime()){  
+    //現在表示中の場合  
+  //  if(wrap !== "wrap"){
+  //    $(this).wrap("<span class='wrap'>");
+  //  }
+  //})
+//});
